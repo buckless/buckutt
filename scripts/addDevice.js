11ea632b94181095dfbd129e7fdc4e3e693446c9
lucas <lucas@buckless.com>
2018-03-05 15:45:11 +0100

    makes admin a user certificate

--- scripts/addDevice.js
+++ scripts/addDevice.js
@@ -24,7 +24,7 @@ function createDir(opts) {
 function setDeviceConfig(opts, fingerprint) {
     log.info('Inserting device in database');
 
-    const device = new models.Device({ name: opts.deviceName, fingerprint });
+    const device = new models.Device({ name: opts.deviceName, fingerprint, isUser: opts.isUser });
     const wiket  = new models.Wiket({
         period_id: opts.periodId,
         point_id : opts.pointId
