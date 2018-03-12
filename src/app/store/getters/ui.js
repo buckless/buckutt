495803fdbb25dd9fe78d5da7c7d81db666d69636
Gabriel JUCHAULT <gabriel@buckless.com>
2018-03-12 16:30:06 +0100

    lint

--- src/app/store/getters/ui.js
+++ src/app/store/getters/ui.js
@@ -5,8 +5,8 @@ export const reloadSum            = state => state.reload.reloads.reduce((a, b) 
 export const tabs                 = state => state.ui.tabs.slice().sort((a, b) => a.name.localeCompare(b.name));
 export const isDegradedModeActive = state => !state.online.status && state.auth.device.event.config.useCardData;
 
-export const isAssignerMode = (state, getters) => !getters.loginState && state.auth.seller.canAssign && !getters.isControllerMode;
+export const isAssignerMode   = (state, getters) => !getters.loginState && state.auth.seller.canAssign && !getters.isControllerMode;
 export const isControllerMode = (state, getters) => !getters.loginState && state.auth.seller.canControl;
-export const isSellerMode   = (state, getters) => !getters.loginState && !getters.isAssignerMode && !getters.isControllerMode && state.auth.seller.canSell;
-export const isReloaderMode = (state, getters) => !getters.loginState && !getters.isAssignerMode && !getters.isControllerMode && state.auth.seller.canReload;
-export const isCashMode     = (_, getters) => getters.isSellerMode || getters.isReloaderMode;
+export const isSellerMode     = (state, getters) => !getters.loginState && !getters.isAssignerMode && !getters.isControllerMode && state.auth.seller.canSell;
+export const isReloaderMode   = (state, getters) => !getters.loginState && !getters.isAssignerMode && !getters.isControllerMode && state.auth.seller.canReload;
+export const isCashMode       = (_, getters) => getters.isSellerMode || getters.isReloaderMode;
