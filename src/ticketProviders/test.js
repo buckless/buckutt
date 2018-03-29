c38a0748798a2acab2ff0c2e0e47636270a4787f
Gabriel JUCHAULT <gabriel@buckless.com>
2018-03-29 16:13:22 +0200

    feat: register from manager

--- src/ticketProviders/test.js
+++ src/ticketProviders/test.js
@@ -1,7 +1,23 @@
-module.exports = ticketId => Promise.resolve({
-    firstname: 'Foo',
-    lastname : 'Bar',
-    mail     : 'foo@bar.com',
-    credit   : 500,
-    ticketId
-});
+module.exports = (ticketOrMail) => {
+    if (ticketOrMail === 'fail') {
+        return Promise.resolve(null);
+    }
+
+    if (ticketOrMail.indexOf('@') > -1) {
+        return Promise.resolve({
+            firstname: 'Foo',
+            lastname : 'Bar',
+            mail     : ticketOrMail,
+            credit   : 500,
+            ticketId : '123123123'
+        });
+    } else {
+        return Promise.resolve({
+            firstname: 'Foo',
+            lastname : 'Bar',
+            mail     : 'foo@bar.com',
+            credit   : 500,
+            ticketId : ticketOrMail
+        });
+    }
+};
