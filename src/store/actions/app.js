8020c47520493bd94fabfaf19f94d23e5a4466a6
lucas <lucas@buckless.com>
2018-04-10 14:09:36 +0200

    chore: remove references to events inside the store

--- src/store/actions/app.js
+++ src/store/actions/app.js
@@ -5,31 +5,6 @@ import routeToRelation from '../../lib/routeToRelation';
  * Global actions
  */
 
-export function updateCurrentEvent({ dispatch }, currentEvent) {
-    const embedEvents = routeToRelation('events');
-
-    return get(`events/${currentEvent.id}?embed=${embedEvents}`).then(result => {
-        if (result.periods) {
-            result.periods = result.periods.filter(period => !period.isRemoved);
-        }
-
-        return dispatch('changeCurrentEvent', result);
-    });
-}
-
-export function changeCurrentEvent({ commit }, currentEvent) {
-    return new Promise(resolve => {
-        localStorage.setItem('event', JSON.stringify(currentEvent));
-        commit('UPDATECURRENTEVENT', currentEvent);
-        resolve();
-    });
-}
-
-export function unselectCurrentEvent({ commit }) {
-    localStorage.removeItem('event');
-    commit('UPDATECURRENTEVENT', null);
-}
-
 export function updateLoggedUser({ commit }, loggedUser) {
     commit('UPDATELOGGEDUSER', loggedUser);
 }
@@ -77,10 +52,6 @@ export function checkAndCreateNeededRouterData({ state, commit, dispatch }) {
         commit('UPDATELOGGEDUSER', JSON.parse(localStorage.getItem('user')));
         dispatch('setToken', localStorage.getItem('token'));
         dispatch('load');
-    }
-
-    if (localStorage.hasOwnProperty('event')) {
-        actions.push(dispatch('updateCurrentEvent', JSON.parse(localStorage.getItem('event'))));
     }
 
     return Promise.all(actions)
