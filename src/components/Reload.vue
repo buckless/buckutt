2b3fdde50cafd33f98fd6b766f73744f23fc3072
Gabriel JUCHAULT <gabriel@buckless.com>
2018-04-11 12:07:37 +0200

    fix(reload): treat errors

--- src/components/Reload.vue
+++ src/components/Reload.vue
@@ -41,7 +41,7 @@
                         <input
                             type="number"
                             class="mdc-text-field__input"
-                            max="100" min="5" step="0.10" pattern="[0-9]*(\.[0-9]+)?"
+                            step="0.10" pattern="[0-9]*(\.[0-9]+)?"
                             error="Veuillez entrer un montant correct"
                             v-model="amount">
                         <span class="mdc-text-field__label">Montant personnalisé</span>
@@ -89,7 +89,15 @@ export default {
             post('reload', { amount: parseInt(amount * 100, 10) })
                 .then(data => {
                     if (data.status) {
-                        this.notify(data.message);
+                        if (data.message.indexOf('Can not reload less than') > -1) {
+                            data.message = data.message.replace('Can not reload less than', 'Rechargement minimal')
+                        }
+
+                        if (data.message.indexOf('Maximum exceeded') > -1) {
+                            data.message = data.message.replace('Maximum exceeded', 'Solde maximal')
+                        }
+
+                        this.notify(data);
 
                         setTimeout(() => {
                             this.loading = false;
@@ -101,6 +109,12 @@ export default {
                     }
                 })
                 .catch(err => {
+                    throw err;
+
+                    setTimeout(() => {
+                        this.loading = false;
+                    }, 200);
+
                     // todo: NaN credit / too much / too small
                 });
         },
