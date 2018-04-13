c4794937d2021aefdace0192b1bade07d14dda99
lucas <lucas@buckless.com>
2018-04-13 11:42:51 +0200

    feat(switch): add switchuser service

---
+++ src/controllers/services/manager/switchuser.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const config = require('../../../config');
+const logger = require('../../../lib/log');
+const rightsDetails = require('../../lib/rightsDetails');
+const dbCatch = require('../../lib/dbCatch');
+const { bookshelf } = require('../../lib/bookshelf');
+const APIError = require('../../errors/APIError');
+
+const log = logger(module);
+
+/**
+ * SearchUser controller.
+ */
+const router = new express.Router();
+
+const tokenOptions = {
+    expiresIn: '30d' // 30 days token
+};
+
+router.post('/services/manager/switchuser', (req, res) => {
+    const infos = { type: req.body.meanOfLogin.toString(), data: req.body.data.toString() };
+    log.info(`Switch to user ${infos.type}(${infos.data})`, infos);
+
+    const errDetails = {
+        mol: infos.type,
+        point: req.Point_id
+    };
+
+    if (req.user.connectType !== 'pin') {
+        return next(new APIError(module, 401, 'User not found', errDetails));
+    }
+
+    const secret = config.app.secret;
+
+    models.MeanOfLogin
+        .query(q =>
+            q.where(bookshelf.knex.raw('lower(data)'), '=', infos.data.toLowerCase().trim())
+        )
+        .where('type', 'in', infos.type.split(','))
+        .where({ blocked: false })
+        .fetch({
+            withRelated: ['user', 'user.rights', 'user.rights.period']
+        })
+        .then(mol => (mol ? mol.toJSON() : null))
+        .then(mol => {
+            if (!mol || !mol.user.id) {
+                return next(new APIError(module, 401, 'User not found', errDetails));
+            }
+
+            user = mol.user;
+
+            if (req.user.mail !== user.mail) {
+                return next(new APIError(module, 401, 'User not found', errDetails));
+            }
+
+            user.pin = '';
+            user.password = '';
+
+            const userRights = rightsDetails(user, req.point_id);
+
+            user.canSell = userRights.sell;
+            user.canReload = userRights.reload;
+            user.canAssign = userRights.assign;
+            user.canControl = userRights.control;
+
+            return res
+                .status(200)
+                .json({
+                    user,
+                    token: jwt.sign(
+                        {
+                            id: user.id,
+                            point: req.point,
+                            event: req.event,
+                            // Will be used by middleware (else how could middleware know if pin or password ?)
+                            connectType
+                        },
+                        secret,
+                        tokenOptions
+                    )
+                })
+                .end();
+        })
+        .catch((err) => {
+            console.log(err);
+            return Promise.reject(err);
+        })
+        .catch(err => dbCatch(module, err, next));
+});
+
+module.exports = router;
