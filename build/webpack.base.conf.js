e1cd82c8ccd8c607ba4a5e8714eb2bbd06451ee3
Gabriel Juchault <gabriel@buckless.com>
2018-04-27 13:15:07 +0000

    Revert "chore(package): dependencies upgrade"
    This reverts commit ef44e8007c0d9320d5f55ba571a951d057090b15

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,7 +2,6 @@
 const path = require('path')
 const utils = require('./utils')
 const config = require('./config')
-const { VueLoaderPlugin } = require('vue-loader')
 const vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
@@ -68,9 +67,6 @@ let base = {
       }
     ]
   },
-  plugins: [
-    new VueLoaderPlugin()
-  ],
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
