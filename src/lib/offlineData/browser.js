030bbc4b314586ef5160e15c1b99ba8a09ee3077
lucas <lucas@buckless.com>
2018-04-28 18:02:10 +0200

    feat(dexie): use only indexeddb

--- src/lib/offlineData/browser.js
+++ src/lib/offlineData/browser.js
@@ -10,7 +10,8 @@ class OfflineData {
     init() {
         this.db.version(1).stores({
             users: 'uid,name,username,barcode,credit',
-            accesses: 'id,cardId,group,start,end'
+            accesses: 'id,cardId,group,start,end',
+            history: 'localId,cardNumber,basketToSend,date,transactionIds'
         });
 
         return Promise.resolve();
@@ -47,6 +48,10 @@ class OfflineData {
         return this.db.accesses.filter(access => access.cardId === cardId).toArray();
     }
 
+    removeHistoryEntry(localId) {
+        return this.db.history.where({ localId }).delete();
+    }
+
     insert(table, data) {
         // dexie wants object and not array, restore from table schema
         if (table === 'users') {
@@ -64,6 +69,14 @@ class OfflineData {
                 group: entry[2],
                 start: entry[3],
                 end: entry[4]
+            }));
+        } else if (table === 'history') {
+            data = data.map(entry => ({
+                localId: entry[0],
+                cardNumber: entry[1],
+                basketToSend: JSON.stringify(entry[2]),
+                date: entry[3],
+                transactionIds: JSON.stringify(entry[4])
             }));
         }
 
--- src/lib/offlineData/index.js
+++ src/lib/offlineData/index.js
@@ -1,5 +1,7 @@
-if (process.env.TARGET === 'electron') {
-    module.exports = require('./browser');
-} else {
-    module.exports = require('./' + process.env.TARGET);
-}
+module.exports = require('./browser');
+
+// if (process.env.TARGET === 'electron') {
+//     module.exports = require('./browser');
+// } else {
+//     module.exports = require('./' + process.env.TARGET);
+// }
