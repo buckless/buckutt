569a6ec1c13a9bc35241d1687cf885b23adf4544
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-07-18 00:46:43 +0200

    feat(axios): add idempotency key header

--- src/app/utils/axios.js
+++ src/app/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import msgpack from 'msgpack-lite';
+import uuid from 'uuid';
 
 const inst = axios.create({
     baseURL: config.api,
@@ -11,6 +12,7 @@ const inst = axios.create({
         (data, headers) => {
             if (data) {
                 headers['Content-Type'] = 'application/x-msgpack';
+                headers['Idempotency-Key'] = uuid();
 
                 return msgpack.encode(data);
             }
