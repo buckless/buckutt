78c2ce691c0bd44cfcb2f8770214a2352ae5cffd
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-07-20 14:58:29 +0200

    fix(dataLoaded): allow user to exit spinner after 7.5sec

--- src/app/components/Loading.vue
+++ src/app/components/Loading.vue
@@ -1,8 +1,31 @@
 <template>
     <div class="b-loading">
         <div class="b-loading__icon"></div>
+        <div class="b-loading__close" v-if="closeable" @click="forceClose">Fermer</div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            closeable: false
+        };
+    },
+
+    mounted() {
+        this.timeout = setTimeout(() => {
+            this.closeable = true;
+        }, 7500);
+    },
+
+    methods: {
+        forceClose() {
+            this.$store.commit('SET_DATA_LOADED', true);
+        }
+    }
+};
+</script>
 
 <style scoped>
 @import '../main.css';
@@ -27,6 +50,20 @@ $loadingSize: 50px;
     margin: 150px auto 0;
     height: $loadingSize;
     width: $loadingSize;
+}
+
+.b-loading__close {
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.24);
+    color: $red;
+    display: inline-block;
+    font-weight: 500;
+    left: 50%;
+    margin-top: 24px;
+    padding: 12px 16px;
+    position: relative;
+    transform: translateX(-50%);
 }
 
 @keyframes spin {
