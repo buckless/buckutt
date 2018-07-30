515eefcfed58864d4c261cb15163293e8c6218c1
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-07-30 18:54:06 +0200

    fix(meansoflogin): allow only one cardId per user

---
+++ migrations/20180730184804_unique_cardId_per_user.js
@@ -0,0 +1,10 @@
+exports.up = function (knex) {
+    return knex.raw(`CREATE UNIQUE INDEX meansoflogin_unique_cardId_per_user_index ON meansoflogin (user_id) WHERE type = 'cardId' AND active = true AND blocked = false`)
+};
+
+exports.down = function (knex) {
+    return knex.schema
+        .table('meansoflogin', (t) => {
+            t.dropIndex('unique_cardId_per_user');
+        });
+};
