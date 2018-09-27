d49f3fff7d5d9f3b7e26c0f0b1faf43825b20a75
Gabriel Juchault <gabriel.juchault@gmail.com>
2018-09-27 21:37:16 +0200

    hotfix: remove currentGroups assigner

--- src/app/utils/formatOfflineResults.js
+++ src/app/utils/formatOfflineResults.js
@@ -1,27 +1,29 @@
 export default users => {
-    const now = new Date();
+    // const now = new Date();
 
     return Promise.all(
-        users.map(user =>
-            window.database.userMemberships(user.id).then(memberships => {
-                const currentGroups = memberships
-                    .filter(
-                        membership =>
-                            new Date(membership.start) <= now && new Date(membership.end) >= now
-                    )
-                    .map(membership => ({ id: membership.group }));
+        users.map(user => {
+            return {
+                credit: user.credit,
+                name: user.name,
+                username: user.username,
+                id: user.userId,
+                molId: user.id,
+                ticketId: user.barcode,
+                physicalId: user.physicalId,
+                currentGroups: []
+            };
+        })
+        // fix me : put back and remove O(n2)
+        //     window.database.userMemberships(user.id).then(memberships => {
+        //         // const currentGroups = memberships
+        //         //     .filter(
+        //         //         membership =>
+        //         //             new Date(membership.start) <= now && new Date(membership.end) >= now
+        //         //     )
+        //         //     .map(membership => ({ id: membership.group }));
 
-                return {
-                    credit: user.credit,
-                    name: user.name,
-                    username: user.username,
-                    id: user.userId,
-                    molId: user.id,
-                    ticketId: user.barcode,
-                    physicalId: user.physicalId,
-                    currentGroups
-                };
-            })
-        )
+        //     })
+        // )
     ).then(users => ({ data: users }));
 };
