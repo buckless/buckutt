8c32667f6c8a3aec4e2498d749f29e8796e7e524
Gabriel Juchault <gabriel@buckless.com>
2019-01-14 15:21:58 +0000

    feat(db): add a button to allow database reset in development mode

--- src/components/events/event/EventShowDetails.vue
+++ src/components/events/event/EventShowDetails.vue
@@ -2,11 +2,15 @@
     <div>
         <h5>Détails de l'événement</h5>
         <b-list :elements="elements" :columns="2"></b-list>
+        <br/>
+        <h5>Zone danger</h5>
+        <mdl-button colored raised @click.native="erase">Remise à zéro</mdl-button>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import { post } from '@/lib/fetch';
 
 export default {
     computed: {
@@ -37,6 +41,31 @@ export default {
                     content: this.focusedEvent.maxPerAccount
                 }
             ];
+        }
+    },
+
+    methods: {
+        async erase() {
+            if (
+                !window.confirm(
+                    "Remettre à zéro la base de données ? Cette opération n'est pas réversible"
+                )
+            ) {
+                return;
+            }
+
+            try {
+                await post('database/erase');
+            } catch (err) {
+                window.alert(
+                    'Impossible de supprimer la base de données (environnement de développement requis)'
+                );
+                console.error(err);
+            }
+
+            window.alert('La base de données à été remise à zéro. Déconexion.');
+
+            this.$router.push('/logout');
         }
     }
 };
