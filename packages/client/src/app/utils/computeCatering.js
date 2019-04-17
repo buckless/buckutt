f98a6762604f002fd9102c5e6879733c06060cdd
Lucas Soulier <lucas@buckless.com>
2019-04-17 21:44:02 +0200

    fix(catering): fix dates filters on catering

--- packages/client/src/app/utils/computeCatering.js
+++ packages/client/src/app/utils/computeCatering.js
@@ -6,11 +6,11 @@ export default (items, configCatering, buyerCatering) => {
         if (!article.start && !article.end) {
             return true;
         } else if (article.start && !article.end) {
-            return article.start <= now;
+            return new Date(article.start) <= now;
         } else if (!article.start && article.end) {
-            return article.end >= now;
+            return new Date(article.end) >= now;
         }
-        return article.start <= now && article.end >= now;
+        return new Date(article.start) <= now && new Date(article.end) >= now;
     });
 
     const remainingBuyerCatering = buyerCatering
