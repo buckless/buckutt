cf9a144a9483dbc2283c7f8d0f1a5d4dd2b6f202
Lucas Soulier <lucas@buckless.com>
2019-06-26 18:25:27 +0200

    fix(encoder): fix options encoder

--- packages/client/src/lib/nfc/coder.js
+++ packages/client/src/lib/nfc/coder.js
@@ -77,13 +77,10 @@ export default () => {
                     }
                 });
 
-                return parseInt(data, 2).toString(16);
+                return parseInt(data.padEnd(usefulDataLength, '0'), 2).toString(16);
             },
             decode: data => {
-                const binaryOptions = parseInt(data, 16)
-                    .toString(2)
-                    .padStart(optionsLength, '0')
-                    .substr(-usefulDataLength);
+                const binaryOptions = parseInt(data, 16).toString(2);
 
                 const options = {
                     assignedCard: binaryOptions.charAt(0) === '1',
